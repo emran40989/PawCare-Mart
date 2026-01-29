3713f43dd78a017c004c5c734adeedb457a7ddb6
emran40989 <emran40989@gmail.com>
2026-01-30 02:45:36 +0600

    category section added with functionality

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -3,17 +3,30 @@ import { Link } from "react-router";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const [category, setCategory] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:3000/services")
+    fetch(`http://localhost:3000/services?category=${category}`)
       .then((res) => res.json())
       .then((data) => setServices(data))
       .catch((err) => console.log(err));
-  }, []);
+  }, [category]);
 
   return (
     <div className="py-8 bg-base-200 rounded-lg">
       <h2 className="text-3xl text-center font-bold my-4">Pets & Supplies</h2>
+      <div className="container mx-auto p-4">
+        <select onChange={(e)=>setCategory(e.target.value)} defaultValue="" className="select">
+          <option value="" disabled>
+            Pick a category
+          </option>
+          <option value="">All</option>
+          <option value="Pets">Pets</option>
+          <option value="Food">Food</option>
+          <option value="Accessories">Accessories</option>
+          <option value="Care Products">Care Products</option>
+        </select>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4 container mx-auto">
         {services.map((service) => (
           <div key={service._id} className="w-full gap-2">
@@ -53,46 +66,6 @@ const Services = () => {
         ))}
       </div>
     </div>
-
-    // <div className="py-8 bg-base-200 rounded-lg">
-    //   <h2 className="text-3xl text-center font-bold my-4">Pets & Supplies</h2>
-    //   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4 container mx-auto">
-    //     {services.map((service) => (
-    //       <div className="w-full gap-2">
-    //         <div className="border border-gray-300 rounded-lg shadow-md bg-white relative overflow-hidden">
-    //           <img
-    //             src={service.image}
-    //             alt={service.title}
-    //             className="w-full h-56 object-cover rounded-2xl p-2"
-    //           />
-    //           <div className="absolute top-3 left-3 bg-black bg-opacity-50 text-white px-2 py-1 rounded-full text-xs">
-    //             {service.category}
-    //           </div>
-    //           <div className="p-3">
-    //             <div className="text-lg font-bold mb-2 text-black">
-    //               {service.serviceName}
-    //             </div>
-    //             <div className="flex justify-between mb-3">
-    //               <div className="text-sm text-black font-semibold">
-    //                 Rating:{" "}
-    //                 <span className="text-yellow-600">{service.rating}</span>
-    //               </div>
-    //               <div className="bg-gray-100 px-2 py-1 rounded text-sm font-semibold text-gray-800">
-    //                 <span className="text-sm font-semibold">Price: </span>{" "}
-    //                 {service.price} USD/hr
-    //               </div>
-    //             </div>
-    //             <Link to={`/details/${service.serviceId}`}>
-    //               <button className="w-full bg-green-500 text-white py-2 rounded hover:bg-green-600 cursor-pointer">
-    //                 View Details
-    //               </button>
-    //             </Link>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     ))}
-    //   </div>
-    // </div>
   );
 };
 
